d94287287aff786ef1c1b7667c0c78c4f88ea791
ganeshgodase4594 <godaseganesh4594@gmail.com>
2023-08-27 19:39:23 +0530

    Basic For Loop Assignment

---
+++ Assignment/Basic_For_Loop_Assignment-2/code10.dart
@@ -0,0 +1,16 @@
+
+// 10. Write a program to print the product of the first 10 numbers
+
+ void main(){
+
+  int mult=1;
+
+  for(int i=1;i<=10;i++){
+
+    mult=mult*i;
+
+  }
+
+  print(mult);
+
+}
---
+++ Assignment/Basic_For_Loop_Assignment-2/code8.dart
@@ -0,0 +1,10 @@
+
+// 7. Write a program to print a table of 12 in reverse order
+
+void main(){
+
+  for(int i=10;i>=1;i--){
+
+    print(12*i);
+  }
+}
