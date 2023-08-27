588b8472cec215ed94182afcaf6fa3d8e651685a
ganeshgodase4594 <godaseganesh4594@gmail.com>
2023-08-27 19:45:05 +0530

    do_while Loop

---
+++ Control_Statement/do-while_Loop/code1.dart
@@ -0,0 +1,12 @@
+
+void main(){
+
+  int i=10;
+
+  do{
+
+    print(i);
+  }while(i<10);
+}
+
+// one time execution....
---
+++ Control_Statement/do-while_Loop/code2.dart
@@ -0,0 +1,33 @@
+
+void main(){
+
+  int x=3;
+
+  do {
+    
+    print("1:C");
+    print("2:CPP");
+    print("3:JAVA");
+    print("4:DART");
+
+    switch(x){
+
+      case 1:
+
+      print("1972");
+
+      case 2:
+
+      print("1978");
+
+      case 3:
+
+      print("1995");
+
+      case 4:
+
+      print("2013");
+    }
+
+  } while (false);
+}
