a537e117200c3627c0da5bd3bf7f891aeedb00c8
ganeshgodase4594 <godaseganesh4594@gmail.com>
2023-08-27 19:45:52 +0530

    For-Loop Code

---
+++ Control_Statement/For_Loop/code1.dart
@@ -0,0 +1,12 @@
+
+// print 20 to 1
+
+void main(){
+
+for(int i=20;i>=1;i--){
+
+  print(i);
+
+}
+
+}
---
+++ Control_Statement/For_Loop/code2.dart
@@ -0,0 +1,13 @@
+// divisible by only 4
+
+void main(){
+
+for(int i=1;i<=20;i++){
+
+  if(i%4==0){
+
+    print(i);
+  }
+}
+
+}
