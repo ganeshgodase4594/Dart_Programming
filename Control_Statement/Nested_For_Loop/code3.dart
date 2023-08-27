e28aae921423cd155f1743a530e3fb75353e6d72
ganeshgodase4594 <godaseganesh4594@gmail.com>
2023-08-27 19:50:09 +0530

    nested_For_Loop

---
+++ Control_Statement/Nested_For_Loop/code3.dart
@@ -0,0 +1,18 @@
+
+import 'dart:io';
+void main(){
+
+  int num=9;
+
+  for(int i=1;i<=3;i++){
+
+    for(int j=1;j<=3;j++){
+
+      stdout.write("${num} ");
+
+      num--;
+    }
+
+    print("");
+  }
+}
