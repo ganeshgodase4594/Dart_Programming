283853d07bed539a5d3f666c4e0c844ad8b9717a
ganeshgodase4594 <godaseganesh4594@gmail.com>
2023-10-07 09:31:37 +0530

    imporant code of getter_setter method

---
+++ Inheritance/code1.dart
@@ -0,0 +1,45 @@
+
+//import 'dart:html';
+//import 'dart:mirrors';
+
+class parent{
+
+  int x=10;
+
+  parent(){
+
+    print("in parent constructor");
+    print(this.hashCode);
+  }
+
+  void printdata(){
+
+    print(x);
+  }
+}
+
+class child extends parent{
+
+  int x=20;
+
+  child(){
+
+    print("in child constructor");
+    print(this.hashCode);
+  }
+
+  void childprintdata(){
+
+    print(x);
+  }
+}
+
+class client{
+
+  void main(){
+
+    child obj=new child();
+    obj.printdata();
+    obj.childprintdata();
+  }
+}
